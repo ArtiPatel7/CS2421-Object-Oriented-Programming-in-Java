71264d281e2e4c1f1d83259ac1b1715b00e58bac
ArtiPatel7 <165051507+ArtiPatel7@users.noreply.github.com>
2024-03-26 13:14:31 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Arti";
+        String lastName = "Patel"; 
+        System.out.println(firstName + " " + lastName);
+    }
+}
